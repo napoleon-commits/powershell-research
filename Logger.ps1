7754cd9868719945d25b3fcc67d7433614acb63a
Byron Hall <byron.hall@ricardodefense.com>
2020-12-23 05:27:47 -0800

    tested logger function

--- Logger.ps1
+++ Logger.ps1
@@ -1,9 +1,22 @@
 function Write-Log {
     Param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [ValidateSet('Error', 'Warning', 'Information', 'Verbose', 'Debug')]
         [string]$LogLevel = 'Information'
     )
 
     switch ($LogLevel) {
-        default { }
+        'Error' { Write-Output "[ERROR]       - $Message" }
+        'Warning' { Write-Output "[WARNING]     - $Message" }
+        'Information' { Write-Output "[INFO] - $Message" }
+        'Verbose' { Write-Output "[VERBOSE]     - $Message" }
+        'Debug' { Write-Output "[DEBUG]       - $Message" }
+        default { throw "Invalid log level: $_" }
     }
 }
+
+Write-Log "Hello World!"
+# Write-Log "" # causes an error
